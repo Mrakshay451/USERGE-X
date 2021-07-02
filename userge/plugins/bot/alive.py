738e663ba89e5791c8104b5c51009dd713d9df54
Mrakshay451 <85699732+Mrakshay451@users.noreply.github.com>
2021-07-02 15:56:34 +0530

    alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -256,11 +256,11 @@ class Bot_Alive:
         return InlineKeyboardMarkup(buttons)
 
     @staticmethod
-    def alive_default_media() -> str:
-        alive_media = [
+    def alive_default_imgs() -> str:
+        alive_imgs = [
             "https://telegra.ph/file/08a71cb88964f7c30fb8f.mp4",
         ]
-        return rand_array(alive_media)
+        return rand_array(alive_imgs)
 
     @staticmethod
     def get_bot_cached_fid() -> str:
